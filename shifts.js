1438517507a2689f16a96aba777390af41bcb3e3
Ryan Acebedo <ryaaanzi777@gmail.com>
2026-09-21 17:44:56 +0800

    Round calculated shift pay - acebedo.ryan

--- shifts.js
+++ shifts.js
@@ -1,9 +1,10 @@
+
 function isValidShift(hours) {
   return hours > 0 && hours <= 24;
 }
 
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+  return Math.round(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
